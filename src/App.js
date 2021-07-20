4d0a14773b803ae71c47362648f9fac61de4e31d
Halyna Sinko <galinasinko14@gmail.com>
2021-07-20 21:02:07 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import ContactForm from "./Components/ContactForm";
+import ContactList from "./Components/ContactList";
+import Filter from "./Components/Filter";
+
+class App extends Component {
+  state = {
+    contacts: [
+      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+    ],
+    filter: "",
+  };
+
+  addNewContact = (newContact) => {
+    // console.log(newContact);
+
+    this.setState((prevState) => ({
+      contacts: [...prevState.contacts, newContact],
+    }));
+  };
+
+  handleChangeFilter = (e) => {
+    this.setState({ filter: e.target.value });
+    // console.log(e);
+  };
+
+  getVisibleContacts = () => {
+    const { contacts, filter } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter((contact) => {
+      // console.log(contact.name);
+      return contact.name.toLowerCase().includes(normalizedFilter);
+    });
+  };
+
+  render() {
+    const { filter } = this.state;
+    const visibleContacts = this.getVisibleContacts();
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.addNewContact} />
+        <h2>Contacts</h2>
+        <Filter value={filter} onChange={this.handleChangeFilter} />
+        <ContactList contacts={visibleContacts} />
+      </div>
+    );
+  }
 }
 
 export default App;
